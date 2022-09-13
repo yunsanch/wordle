ba921156281f0d7781c39dc5bd0ece432bbaa3b7
yunior sanchez <ygar0612@fmuniv.edu>
2022-09-12 22:07:21 -0400

    Initial commit

---
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.wordle
+
+import android.content.Context
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.SpannableString
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var wordToGuess : String
+    private var countRound = 0
+    private var Streak = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        wordToGuess = FourLetterWordList.FourLetterWordList.getRandomFourLetterWord()
+
+        val Button = findViewById<Button>(R.id.GuessButton)
+        val Guess1 = findViewById<TextView>(R.id.FirstGuessTextView1)
+        val Guess2 = findViewById<TextView>(R.id.secondGuessTextView5)
+        val Guess3 = findViewById<TextView>(R.id.ThirdGuessTextView9)
+        val guessCheckuno = findViewById<TextView>(R.id.Guesscheck1TextView3)
+        val guessCheckdos = findViewById<TextView>(R.id.Guesscheck2TextView7)
+        val guessChecktres = findViewById<TextView>(R.id.Guesscheck3TextView11)
+        val worduno = findViewById<TextView>(R.id.word1TextView2)
+        val wordunocheck = findViewById<TextView>(R.id.word1checkTextView4)
+        val worddos = findViewById<TextView>(R.id.word2TextView6)
+        val worddoscheck = findViewById<TextView>(R.id.word2checkTextView8)
+        val wordtres = findViewById<TextView>(R.id.word3TextView10)
+        val wordtresCheck = findViewById<TextView>(R.id.word3checkTextView12)
+        val WordtoguessTextview = findViewById<TextView>(R.id.WordToGuessTextview13)
+        val simpleEditText = findViewById<EditText>(R.id.inputGuess)
+
+
+        Button.setOnClickListener {
+            it.hideKeyboard()
+            val strValue = simpleEditText.text.toString().uppercase()
+//            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+            counter ++
+            WordtoguessTextview.text = wordToGuess
+            if (counter == 1) {
+                worduno.text = strValue
+                worduno.visibility = View.VISIBLE
+                Guess1.visibility = View.VISIBLE
+                guessCheckuno.visibility = View.VISIBLE
+                wordunocheck.visibility = View.VISIBLE
+                wordunocheck.text = worduno.toString()
+                wordunocheck.text = checkGuess(strValue)
+                simpleEditText.text.clear()
+            }
+            if (counter == 2) {
+                worddos.text = strValue
+                worddos.visibility = View.VISIBLE
+                Guess2.visibility = View.VISIBLE
+                worddoscheck.visibility = View.VISIBLE
+                guessCheckdos.visibility =View.VISIBLE
+                worddoscheck.text = worddos.toString()
+                worddoscheck.text = checkGuess(strValue)
+                simpleEditText.text.clear()
+            }
+            if (counter == 3) {
+                wordtres.text = strValue
+                wordtres.visibility = View.VISIBLE
+                Guess3.visibility = View.VISIBLE
+                wordtresCheck.visibility = View.VISIBLE
+                guessChecktres.visibility = View.VISIBLE
+                wordtresCheck.text = wordtres.toString()
+                wordtresCheck.text = checkGuess(strValue)
+//                Toast.makeText(this, "Max attends is 3", Toast.LENGTH_SHORT).show()
+                WordtoguessTextview.visibility = View.VISIBLE
+                Button.setBackgroundColor(Color.GRAY)
+                Button.isClickable = false
+            }
+
+
+        }
+
+    }
+    fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
+
+    private fun checkGuess(guess: String) : String {
+        var result = ""
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i]) {
+                result += "O"
+            }
+            else if (guess[i] in wordToGuess) {
+                result += "+"
+            }
+            else {
+                result += "X"
+            }
+        }
+        return result
+
+    }
+
+    var counter = 0
+//
+
+}
